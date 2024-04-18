6ad1f16a5a2815251badfe62a8eb0a4dbb88f544
irakli maisuradze <irakli.maisuradze31.19@gmail.com>
2024-04-18 14:36:26 +0400

    integreated react-emoji and message handler

--- src/components/home/message-input.tsx
+++ src/components/home/message-input.tsx
@@ -1,20 +1,67 @@
 import { Laugh, Mic, Plus, Send } from "lucide-react";
 import { Input } from "../ui/input";
-import { useState } from "react";
+import React, { useState } from "react";
 import { Button } from "../ui/button";
+import { useMutation, useQuery } from "convex/react";
+import { api } from "../../../convex/_generated/api";
+import { useConversationStore } from "@/store/chat-store";
+import { toast } from "sonner";
+import EmojiPicker, { Theme } from "emoji-picker-react";
+import useComponentVisible from "@/hooks/useComponentsVisible";
 
 const MessageInput = () => {
   const [msgText, setMsgText] = useState("");
+  const { ref, isComponentVisible, setIsComponentVisible } =
+    useComponentVisible(false);
+  const sendTextMsg = useMutation(api.messages.sendTextMessage);
+  const me = useQuery(api.users.getMe);
+  const { selectedConversation } = useConversationStore();
+
+  const handleSendTextMsg = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    try {
+      await sendTextMsg({
+        content: msgText,
+        conversation: selectedConversation!._id,
+        sender: me!._id,
+      });
+      setMsgText("");
+    } catch (err) {
+      toast.error("Message Failed");
+    }
+  };
 
   return (
     <div className="bg-gray-primary p-2 flex gap-4 items-center">
       <div className="relative flex gap-2 ml-2">
-        {/* EMOJI PICKER WILL GO HERE */}
-        <Laugh className="text-gray-600 dark:text-gray-400" />
+        <div
+          ref={ref}
+          onClick={() => setIsComponentVisible(true)}
+          className=" cursor-pointer"
+        >
+          {isComponentVisible && (
+            <EmojiPicker
+              theme={Theme.DARK}
+              onEmojiClick={(emojiObject) => {
+                setMsgText((prev) => prev + emojiObject.emoji);
+              }}
+              style={{
+                position: "absolute",
+                bottom: "1.5rem",
+                left: "1rem",
+                zIndex: 50,
+                cursor: "pointer",
+              }}
+            />
+          )}
+          <Laugh className="text-gray-600 dark:text-gray-400" />
+        </div>
+
         <Plus className="text-gray-600 dark:text-gray-400" />
       </div>
       <Plus />
-      <form className="w-full flex gap-3">
+      <form className="w-full flex gap-3" onSubmit={handleSendTextMsg}>
         <div className="flex-1">
           <Input
             type="text"
